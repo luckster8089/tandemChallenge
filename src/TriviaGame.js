5c608418bbfb8a26ed3abde44d34e122fa877e12
luckster8089 <ly.lucky88@gmail.com>
2020-10-28 13:18:21 -0500

    Generated randomQuestion from jsonData

--- src/TriviaGame.js
+++ src/TriviaGame.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
 export default function TriviaGame({ data }) {
+    const rand = Math.floor(Math.random() * data.length)
+    const randomQuestion = data[rand]
+    console.log(randomQuestion)
+    // Randomize Array
+    // Loop and pick 10 questions 
+    // Separate into new array 
     return (
         <div>
             
