699e0c1394be39e5d03d9301c798aeaf6c75cc14
luckster8089 <ly.lucky88@gmail.com>
2020-10-28 12:09:53 -0500

    Added material ui and created Trivia component

--- src/Trivia.js
+++ src/Trivia.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Button from '@material-ui/core/Button'
+import TriviaGame from './TriviaGame'
 
 export default function Trivia({ questions, answers, incorrectAnswers }) {
     return (
         <div>
             <h2>Are You Ready For Tandem Trivia?</h2>
+            <Button variant="contained" color="primary">
+                Yes
+            </Button>
+            <Button variant="contained" color="secondary">
+                No
+            </Button>
         </div>
     )
 }
