fa4af8e8a034d97e09158b0f02005a3ffd09aab4
luckster8089 <ly.lucky88@gmail.com>
2020-10-28 12:14:00 -0500

    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,14 +2,6 @@ import jsonData from './triviaQuestions.json';
 import Trivia from './Trivia';
 
 function App() {
-  const trivia = jsonData.map(questions => (
-    <Trivia
-      questions={questions.questions}
-      answers={questions.correct}
-      incorrectAnswers={questions.incorrect}
-    />
-  ))
-
   return (
     <div className="App">
       <Trivia data={jsonData} />
--- src/Trivia.js
+++ src/Trivia.js
@@ -2,11 +2,19 @@ import React from 'react'
 import Button from '@material-ui/core/Button'
 import TriviaGame from './TriviaGame'
 
-export default function Trivia({ questions, answers, incorrectAnswers }) {
+export default function Trivia({ data }) {
+    const triviaGame = data.map(questions => (
+        <TriviaGame
+          questions={questions.questions}
+          answers={questions.correct}
+          incorrectAnswers={questions.incorrect}
+        />
+      ))
+
     return (
         <div>
             <h2>Are You Ready For Tandem Trivia?</h2>
-            <Button variant="contained" color="primary">
+            <Button variant="contained" color="primary" onClick={() => {triviaGame}}>
                 Yes
             </Button>
             <Button variant="contained" color="secondary">
