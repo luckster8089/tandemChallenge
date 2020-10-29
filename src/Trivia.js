0401375b28c42e9b2e417e37e88086ea5d0b9bc4
luckster8089 <ly.lucky88@gmail.com>
2020-10-29 12:30:04 -0500

    Styles Trivia Component

--- src/Trivia.js
+++ src/Trivia.js
@@ -1,19 +1,45 @@
 import React from 'react';
+import { withStyles } from '@material-ui/styles';
 import { useHistory } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 
-export default function Trivia() {
+const styles = {
+  root: {
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    width: "12.5%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  button: {
+    width: "50%",
+    margin: ".5rem",
+    borderRadius: ".8rem"
+  }
+}
+
+function Trivia({ classes }) {
     const history = useHistory();
 
     return (
-        <div>
+        <div className={classes.root}>
             <h2>Are You Ready For Tandem Trivia?</h2>
-            <Button variant="contained" color="primary" onClick={() => history.push('/tandemTrivia')}>
+            <div className={classes.buttonContainer}>
+            <Button variant="contained" className={classes.button} color="primary" onClick={() => history.push('/tandemTrivia')}>
                 Yes
             </Button>
-            <Button variant="contained" color="secondary">
+            <Button variant="contained" className={classes.button} color="secondary">
                 No
             </Button>
+            </div>
         </div>
     )
 }
+
+export default withStyles(styles)(Trivia);
--- src/TriviaQuestions.js
+++ src/TriviaQuestions.js
@@ -2,14 +2,19 @@ import React, { useState, useEffect } from 'react'
 import Button from '@material-ui/core/Button';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
+import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
+import { withStyles } from '@material-ui/core/styles';
+import styles from './styles/TriviaQuestionsStyles'
 
-export default function TriviaQuestions({ questions }) {
+function TriviaQuestions({ questions, classes }) {
     const [questionNumber, setQuestionNumber] = useState(0)
     const [value, setValue] = useState('')
     const [score, setScore] = useState(0)
+    const [helperText, setHelperText] = useState('You got this!')
+    const [error, setError] = useState(false)
     const [question, setQuestion] = useState({
       questionName: '',
       answers: []
@@ -23,9 +28,13 @@ export default function TriviaQuestions({ questions }) {
     function handleSubmit(e){
         e.preventDefault();
         if (value === questions[questionNumber].correct) {
-          setScore(currScore => currScore + 1)
+          setHelperText('You are correct!');
+          setScore(currScore => currScore + 1);
+          setError(false);
+        } else if (value !== questions[questionNumber].correct) {
+          setHelperText(`Sorry, wrong answer! The correct answer is ${questions[questionNumber].correct}`);
+          setError(true);
         }
-
         setQuestionNumber(currQuestion => currQuestion + 1)
         setQuestion({questionName: questions[questionNumber].question, answers: questions[questionNumber].incorrect})
     }
@@ -42,18 +51,21 @@ export default function TriviaQuestions({ questions }) {
 
 
     return (
-        <div>
-            <h2>You've Completed {questionNumber} / 10 </h2>
+        <div className={classes.questions}>
+            <h2>You've Completed {questionNumber} / 10 Questions</h2>
             <h2>Your Score: {score}</h2>
-            <h3>{question.questionName}</h3>
+            <h3 className="questions">{question.questionName}</h3>
             <form onSubmit={handleSubmit}>
-              <FormControl component="fieldset">
+              <FormControl component="fieldset" error={error}>
               <FormLabel component="legend">Select an Answer</FormLabel>
               <RadioGroup aria-label="quiz" name="quiz" value={value} onChange={handleChange}>
                 {question.answers.map((q, index) => (
+                  <div className={classes.questions}>
                   <FormControlLabel key={index} value={q} control={<Radio />} label={q}/>
+                  </div>
                 ))}
               </RadioGroup>
+              <FormHelperText>{helperText}</FormHelperText>
               <Button type="submit" variant="contained" color="primary">
                 Submit
               </Button>
@@ -63,3 +75,5 @@ export default function TriviaQuestions({ questions }) {
         </div>
     )
 }
+
+export default withStyles(styles)(TriviaQuestions)
