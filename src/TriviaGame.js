99d4f08ea48f152feafbe9aad0f1a743cd97239e
luckster8089 <ly.lucky88@gmail.com>
2020-10-28 19:50:30 -0500

    Added form and score

--- src/TriviaGame.js
+++ src/TriviaGame.js
@@ -1,28 +1,14 @@
-import React, {useState} from 'react';
+import React from 'react';
 import TriviaQuestions from './TriviaQuestions';
 
 
 export default function TriviaGame({ data }) {
-
     const newArr = [];
     while (newArr.length <= 9) {
       let rand = Math.floor(Math.random() * data.length)
       let randomQuestion = data[rand]
       if (newArr.indexOf(randomQuestion) === -1)newArr.push(randomQuestion)
     }
-
-    // const questions = newArr.map((question) => (
-    //   <TriviaQuestions
-    //     key={question.question}
-    //     question={question.question}
-    //     answers={question.correct}
-    //     incorrect={question.incorrect}
-    //     incrementQuestionCount={incrementQuestionCount}
-    //     q={newArr}
-    //   />
-    // ))
-
-
 
     return (
         <div>
--- src/TriviaQuestions.js
+++ src/TriviaQuestions.js
@@ -9,6 +9,7 @@ import FormLabel from '@material-ui/core/FormLabel';
 export default function TriviaQuestions({ questions }) {
     const [questionNumber, setQuestionNumber] = useState(0)
     const [value, setValue] = useState('')
+    const [score, setScore] = useState(0)
     const [question, setQuestion] = useState({
       questionName: '',
       answers: []
@@ -18,31 +19,35 @@ export default function TriviaQuestions({ questions }) {
         setValue(e.target.value)
     }
 
-
-    function incrementQuestionCount() {
-      setQuestionNumber(currQuestion => currQuestion + 1)
-      setQuestion({questionName: questions[questionNumber].question, answers: questions[questionNumber].incorrect})
+    function handleSubmit(e){
+        e.preventDefault();
+        if (value === questions[questionNumber].correct) {
+          setScore(currScore => currScore + 1)
+        }
+        setQuestionNumber(currQuestion => currQuestion + 1)
+        setQuestion({questionName: questions[questionNumber].question, answers: questions[questionNumber].incorrect})
     }
     
     useEffect(() => {
       const question = questions[questionNumber].question
       const answers = questions[questionNumber].incorrect.concat(questions[questionNumber].correct)
-      setQuestion({questionName: question, answers: answers})
-    }, [questionNumber])
+      setQuestion({ questionName: question, answers: answers })
+    }, [questionNumber, questions])
 
 
     return (
         <div>
+            <h2>Your Score: {score}</h2>
             <h3>{question.questionName}</h3>
             <form onSubmit={handleSubmit}>
             <FormControl component="fieldset">
             <FormLabel component="legend">Select an Answer</FormLabel>
-            <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
-              {question.answers.map((q) => (
-                <FormControlLabel value={q} control={<Radio />} label={q}/>
+            <RadioGroup aria-label="quiz" name="quiz" value={value} onChange={handleChange}>
+              {question.answers.map((q, index) => (
+                <FormControlLabel key={index} value={q} control={<Radio />} label={q}/>
               ))}
             </RadioGroup>
-            <Button type="submit" variant="contained" color="primary" onClick={incrementQuestionCount}>
+            <Button type="submit" variant="contained" color="primary">
                 Submit
             </Button>
           </FormControl>
